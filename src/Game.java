9d597450475ea4d0d0c5fb9816cd801863d4d404
Nishant Bhasin <nikieme3@gmail.com>
2012-10-18 22:39:16 -0400

    Minor Changes in GAME.java

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,4 @@
-/**
- *  This class is the main class of the "World of Zuul" application. 
- *  "World of Zuul" is a very simple, text based adventure game.  Users 
- *  can walk around some scenery. That's all. It should really be extended 
- *  to make it more interesting!
- * 
- *  To play this game, create an instance of this class and call the "play"
- *  method.
- * 
- *  This main class creates and initialises all the others: it creates all
- *  rooms, creates the parser and starts the game.  It also evaluates and
- *  executes the commands that the parser returns.
- * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2011.08.10
- */
+//RUN THE CODE FROM HERE... !!
 
 public class Game 
 {
